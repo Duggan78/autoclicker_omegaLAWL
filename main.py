47d9ea34a56801e5ae1ffa28ad004d716d90d590
Jack Duggan <53382645+Duggan78@users.noreply.github.com>
2020-07-31 07:04:23 -0400

    I forget.
    I made changes but I forget what changes

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import keyboard
 import mouse
 
 # TODO: when a key is pressed trigger autoclicker( some type of loop)
-# when a different key is break loop.
+# when a different key is pressed break loop.
 
 clicks = 0
 
 if keyboard.read_key('p'):
     press_true = 'true'
-    if press_true == 'true':
-        while press_true == 'true' and clicks <= 100:
-            mouse.click('left')
-            clicks = clicks + 1
-            print(int(clicks))
+    while press_true == 'true' and clicks <= 10000:
+        mouse.click('left')
+        clicks = clicks + 1
+        print(int(clicks))
+
+
 
