fb4025bb2d7eb183e3f3ed3b675443bb7eeb5511
Jack Duggan <53382645+Duggan78@users.noreply.github.com>
2020-07-30 20:54:52 -0400

    added fail safe
    if over 100 clicks happens before being stopped the program will shut off.

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import mouse
 # TODO: when a key is pressed trigger autoclicker( some type of loop)
 # when a different key is break loop.
 
+clicks = 0
+
 if keyboard.read_key('p'):
     press_true = 'true'
     if press_true == 'true':
-        while press_true == 'true':
+        while press_true == 'true' and clicks >= 100:
             mouse.click('left')
-            print("Hello World")
-        if keyboard.read_key('o'):
-            press_true = 'false'
+            clicks = clicks + 1
+            print(int(clicks))
+
